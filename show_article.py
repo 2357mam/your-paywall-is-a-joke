ab3053c59e478dea1d7bbcd798dc059f5f8e2da8
Maximilian Middeke <2357mam@gmail.com>
2022-05-20 13:51:53 +0200

    init

---
+++ show_article.py
@@ -0,0 +1,26 @@
+import json
+import sys
+import requests
+
+if __name__ == "__main__":
+    try:
+        url = sys.argv[1]
+    except:
+        sys.exit("provide arg 1, an url to a kn-article")
+    try:
+        html = requests.get(url).text
+    except:
+        sys.exit(f"could not curl {url}")
+    try:
+        article_dict_str = html.split("<script type=\"application/ld+json\">")[1].split("</script>")[0]
+    except:
+        sys.exit("could not split html-source as expected")
+    try:
+        article_dict = json.loads(article_dict_str)
+    except:
+        sys.exit("could not load article-json as expected")
+
+    article_headline = article_dict["headline"]
+    article_body = article_dict["articleBody"]
+
+    print(article_headline, "\n", article_body)
